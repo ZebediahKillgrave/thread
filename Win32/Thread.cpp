e050bc6ef3dfe763e9e775c18a79e4817341b2b2
Florent Espanet <florent.esp@gmail.com>
2014-01-07 17:13:58 +0100

    implemented windows version

--- Win32/Thread.cpp
+++ Win32/Thread.cpp
@@ -1,45 +1,16 @@
 #if defined(_WIN32) || defined(__WIN32__)
 
-#include "Socket.hpp"
+#include <windows.h>
+#include "Thread.hpp"
 
-int Socket::socket(int domain, int type, int protocol)
+int create(void *thread, void *(*start_routine) (void *), void *arg)
 {
-  return (0);
+    return ((int)CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)start_routine, arg, 0, (LPDWORD)thread));
 }
 
-int Socket::connect(int sockfd, const void *addr, int addrlen)
+void exit(void *reval)
 {
-  return (0);
-}
-
-int Socket::send(int sockfd, const void *buf, int len, int flags)
-{
-  return (0);
-}
-
-int Socket::receive(int sockfd, void *buf, int len, int flags)
-{
-  return (0);
-}
-
-int Socket::bind(int sockfd, const void *addr, int addrlen)
-{
-  return (0);
-}
-
-int Socket::listen(int sockfd, int backlog)
-{
-  return (0);
-}
-
-int Socket::accept(int sockfd, void *addr, int *addrlen)
-{
-  return (0);
-}
-
-int Socket::close(int sockfd)
-{
-  return (0);
+    ExitThread((DWORD)reval);
 }
 
 #endif
